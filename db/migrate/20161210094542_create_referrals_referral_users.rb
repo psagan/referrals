b480c8c25a7b9995306f14eb450f80b02f7e5112
psagan <patryk.sagan@icloud.com>
2016-12-11 08:03:31 +0100

    Update migration for referral users

--- db/migrate/20161210094542_create_referrals_referral_users.rb
+++ db/migrate/20161210094542_create_referrals_referral_users.rb
@@ -2,7 +2,7 @@ class CreateReferralsReferralUsers < ActiveRecord::Migration
   def change
     create_table :referrals_referral_users do |t|
       t.integer :user_id, null: false, index: true, unique: true
-      t.references :partner, foreign_key: true, null: false
+      t.integer :partner_id, null: false, index: true
 
       t.timestamps
     end
