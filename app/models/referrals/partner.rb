07f34b9f027cfc2ed4381e0caa71878ca6bcc2a6
psagan <patryk.sagan@icloud.com>
2016-12-11 08:27:19 +0100

    Naming changes

--- app/models/referrals/partner.rb
+++ app/models/referrals/partner.rb
@@ -1,7 +1,7 @@
 module Referrals
   class Partner < ApplicationRecord
-    has_many :referrals_referral_users
-    has_many :referrals, through: :referrals_referral_users
+    has_many :referral_users
+    has_many :referrals, through: :referral_users
     belongs_to :user, class_name: Referrals.user_class, foreign_key: :user_id
 
     validates :user, uniqueness: true, presence: true
