b0d2f7d8649c8b7765c83b9a7d2978f56a3cd620
psagan <patryk.sagan@icloud.com>
2016-12-21 10:06:33 +0100

    Fix engine module hash syntax

--- lib/referrals/engine.rb
+++ lib/referrals/engine.rb
@@ -5,7 +5,7 @@ module Referrals
     # use rspec in rails engine http://bit.ly/2hmeFFY
     config.generators do |g|
       g.test_framework :rspec
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
