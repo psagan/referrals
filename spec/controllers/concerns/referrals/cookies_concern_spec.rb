f2b0c2e596ce254384d0da299f6f16b1683bb878
psagan <patryk.sagan@icloud.com>
2016-12-22 08:10:04 +0100

    Add some tests for cookies_concern

--- spec/controllers/concerns/referrals/cookies_concern_spec.rb
+++ spec/controllers/concerns/referrals/cookies_concern_spec.rb
@@ -14,26 +14,55 @@ RSpec.describe Referrals::CookiesConcern, type: :controller do
     context "when no pid in request" do
       before { get :fake_action }
       it "does not set referral cookie" do
-        expect(response.cookies[:referrals_pid]).to eq(nil)
+        expect(response.cookies['referrals_pid']).to eq(nil)
       end
     end
 
     context "when pid in request" do
       context "when no cookie set" do
         context "when partner exists" do
-          it "" do
-            FactoryGirl.create(:partner)
+          let!(:partner) { FactoryGirl.create(:partner) }
+
+          it "set cookie value to partner id" do
+            get :fake_action, params: { pid: partner.id }
+
+            expected = {
+                value: partner.id,
+                expires: 1.year.from_now
+            }
+            expect(response.cookies['referrals_pid']).to eq(partner.id.to_s)
           end
+
+          it "set cookie expiration time" do
+            date = 1.year.from_now
+            allow_any_instance_of(ActiveSupport::Duration).to receive(:from_now).and_return(date)
+            # thanks to http://bit.ly/2hXQka9
+            stub_cookie_jar = HashWithIndifferentAccess.new
+            allow(controller).to receive(:cookies).and_return(stub_cookie_jar)
+
+            get :fake_action, params: { pid: partner.id }
+
+            expect(stub_cookie_jar['referrals_pid'][:expires]).to eq(date)
+          end
+
         end
 
         context "when partner does not exist" do
+          it "does not set cookie" do
+            get :fake_action, params: { pid: 7 }
 
+            expect(response.cookies['referrals_pid']).to eq(nil)
+          end
         end
       end
 
       context "when cookie set" do
 
       end
+    end
+
+    context "when no pid in request" do
+
     end
   end
 end
