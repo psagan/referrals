3b8bf34ed3dfcf89ec75cc7438c7fd5bf8d0c150
psagan <patryk.sagan@icloud.com>
2016-12-23 07:39:03 +0100

    Explanation + Code Optimalization

--- app/controllers/concerns/referrals/operations_concern.rb
+++ app/controllers/concerns/referrals/operations_concern.rb
@@ -3,7 +3,10 @@ module Referrals
 
     def assign_referral_to_partner(referral)
       return unless pid = cookies[:referrals_pid]
-      # using find_by_id
+      # using find_by id to not raise exception when partner not found
+      # possible scenario that user has old cookie and partner is no longer partner
+      # so we don't want to raise exceptions on production because of such
+      # scenario
       partner = ::Referrals::Partner.find_by(id: pid)
       partner.referrals << referral if partner
     end
