8c3a05c152939ebe5c6c04e35f78f8e7b295c10e
psagan <patryk.sagan@icloud.com>
2016-12-27 11:08:34 +0100

    Association for Referrable

--- app/models/concerns/referrals/referable.rb
+++ app/models/concerns/referrals/referable.rb
@@ -4,6 +4,7 @@ module Referrals
 
     included do
       has_one :partnership, class_name: 'Referrals::Partner'
+      has_one :referral_user, class_name: "Referrals::ReferralUser"
     end
 
     def partner?
