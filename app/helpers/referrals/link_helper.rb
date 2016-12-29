d3beeb580bbe31ce345af871662821e1edaa344c
psagan <patryk.sagan@icloud.com>
2016-12-29 16:52:59 +0100

    Add some todos

--- app/helpers/referrals/link_helper.rb
+++ app/helpers/referrals/link_helper.rb
@@ -1,7 +1,8 @@
 module Referrals
   module LinkHelper
     def referral_link(partner)
-      # @todo - make configurable
+      # @todo - make configurable host and pid, if not config option use default
+      # @todo - use request.protocol + request.host when no config option defined
       request.protocol + request.host.chomp('/') + '/?pid=' + partner.id.to_s
     end
   end
