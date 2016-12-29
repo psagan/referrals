690c70aafaab1d7393dacb00ec41237c2e900636
psagan <patryk.sagan@icloud.com>
2016-12-29 14:36:10 +0100

    Add Income History related controller, view and filter action

---
+++ app/controllers/referrals/income_history_controller.rb
@@ -0,0 +1,32 @@
+module Referrals
+  class IncomeHistoryController < ApplicationController
+    def index
+      @date_from = get_date(:date_from)
+      @date_to = get_date(:date_to)
+      @income_histories = Referrals::IncomeHistory
+                              .by_partner(current_user.partnership)
+                              .date_from(@date_from)
+                              .date_to(@date_to)
+                              .page(params[:page])
+
+    end
+
+    def filter
+      @date_from = get_date(:date_from)
+      @date_to = get_date(:date_to)
+      redirect_to income_history_index_path(date_from: @date_from, date_to: @date_to)
+    end
+
+    private
+
+    def current_user
+      User.first
+    end
+
+    def get_date(key)
+      Date.parse(params[key])
+    rescue
+      nil
+    end
+  end
+end
--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,18 +1,24 @@
 # create users
-User.create(email: 'partner@example.com')
-partner = User.first
-4.times do |n|
-  User.create(email: "m#{n}@example.com")
-  User.last.tap do |u|
-    partner.reload.partnership.referrals << u
-    Referrals::IncomeHistory.create(
-        referral: u,
-        partner: partner.partnership,
-        info: 'Payment for subscription',
-        amount: 2077,
-        share: partner.partnership.share,
-        share_amount: partner.partnership.share * 2077
-    )
+q = 10
+2.times do |i|
+  email = "partner#{i}@example.com"
+  User.create(email: email)
+  partner = User.find_by_email(email).tap {|u| u.make_partner!}
+  q.times do |n|
+    created_at = n < (q/2) ? 1.month.ago : DateTime.now
+    User.create(email: "m#{i}_#{n}@example.com")
+    User.last.tap do |u|
+      partner.reload.partnership.referrals << u
+      Referrals::IncomeHistory.create(
+          referral: u,
+          partner: partner.partnership,
+          info: 'Payment for subscription',
+          amount_cents: 2077,
+          share: partner.partnership.share,
+          share_amount_cents: partner.partnership.share * 2077,
+          created_at: created_at
+      )
+    end
   end
 end
 
