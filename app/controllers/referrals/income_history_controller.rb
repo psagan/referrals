ba688f8e6e7c8b631e2fd53f6306930e8444800b
psagan <patryk.sagan@icloud.com>
2016-12-29 14:37:22 +0100

    Small refactoring in income history controller

--- app/controllers/referrals/income_history_controller.rb
+++ app/controllers/referrals/income_history_controller.rb
@@ -4,17 +4,15 @@ module Referrals
       @date_from = get_date(:date_from)
       @date_to = get_date(:date_to)
       @income_histories = Referrals::IncomeHistory
-                              .by_partner(current_user.partnership)
-                              .date_from(@date_from)
-                              .date_to(@date_to)
-                              .page(params[:page])
+        .by_partner(current_user.partnership)
+        .date_from(@date_from)
+        .date_to(@date_to)
+        .page(params[:page])
 
     end
 
     def filter
-      @date_from = get_date(:date_from)
-      @date_to = get_date(:date_to)
-      redirect_to income_history_index_path(date_from: @date_from, date_to: @date_to)
+      redirect_to income_history_index_path(date_from: get_date(:date_from), date_to: get_date(:date_to))
     end
 
     private
