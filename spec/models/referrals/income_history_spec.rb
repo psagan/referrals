83e971e78a549f35df883693f6d70894569735ab
psagan <patryk.sagan@icloud.com>
2016-12-30 17:14:16 +0100

    Fix Spec

--- spec/models/referrals/income_history_spec.rb
+++ spec/models/referrals/income_history_spec.rb
@@ -4,6 +4,14 @@ require 'support/shared/monetize_attr'
 module Referrals
   RSpec.describe IncomeHistory, type: :model do
 
+    shared_context :empty_scope do |method|
+      it "returns all data" do
+        results = subject
+
+        expect(results).to eq([income_history_1, income_history_2, income_history_3])
+      end
+    end
+
     describe "#amount" do
       include_examples :monetize_attr, :amount
     end
@@ -19,40 +27,33 @@ module Referrals
       let!(:income_history_1) { FactoryGirl.create(:income_history, partner: partner, referral: user, created_at: 2.months.ago) }
       let!(:income_history_2) { FactoryGirl.create(:income_history, partner: partner, referral: user, created_at: 1.month.ago) }
       let!(:income_history_3) { FactoryGirl.create(:income_history, partner: partner, referral: user) }
+      let(:date) { 1.month.ago }
 
       describe ".date_from" do
+        subject { Referrals::IncomeHistory.by_date_from(date) }
         context "when date_from provided" do
           it "returns proper data by date_from" do
-            results = Referrals::IncomeHistory.by_date_from(1.month.ago)
-
-            expect(results).to eq([income_history_2, income_history_3])
+            expect(subject).to eq([income_history_2, income_history_3])
           end
         end
 
-        context "when date_from not provided" do
-          it "returns all data" do
-            results = Referrals::IncomeHistory.by_date_from(nil)
-
-            expect(results).to eq([income_history_1, income_history_2, income_history_3])
-          end
+        context "when date_from is nil" do
+          let(:date) { nil }
+          it_behaves_like :empty_scope
         end
       end
 
       describe ".date_to" do
+        subject { Referrals::IncomeHistory.by_date_to(date) }
         context "when date_to provided" do
           it "returns proper data by date_to" do
-            results = Referrals::IncomeHistory.by_date_to(1.month.ago)
-
-            expect(results).to eq([income_history_1, income_history_2])
+            expect(subject).to eq([income_history_1, income_history_2])
           end
         end
 
         context "when date_to not provided" do
-          it "returns all data" do
-            results = Referrals::IncomeHistory.by_date_from(nil)
-
-            expect(results).to eq([income_history_1, income_history_2, income_history_3])
-          end
+          let(:date) { nil }
+          it_behaves_like :empty_scope
         end
       end
 
