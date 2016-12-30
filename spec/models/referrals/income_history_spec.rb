46ee22847fcc3803c2bf3d60d948e53457295093
psagan <patryk.sagan@icloud.com>
2016-12-30 14:17:20 +0100

    Update IncomeHistory spec

--- spec/models/referrals/income_history_spec.rb
+++ spec/models/referrals/income_history_spec.rb
@@ -23,24 +23,56 @@ module Referrals
         let!(:income_history_3) { FactoryGirl.create(:income_history, partner: partner, referral: user) }
 
         context "when date_from provided" do
-          it "returns proper data" do
-            results = Referrals::IncomeHistory.by_date_from(1.month.ago).map(&:id)
+          it "returns proper data by date_from" do
+            results = Referrals::IncomeHistory.by_date_from(1.month.ago)
 
-            expect(results).to eq([income_history_2.id, income_history_3.id])
+            expect(results).to eq([income_history_2, income_history_3])
           end
         end
 
         context "when date_from not provided" do
           it "returns all data" do
-            results = Referrals::IncomeHistory.by_date_from(nil).map(&:id)
+            results = Referrals::IncomeHistory.by_date_from(nil)
 
-            expect(results).to eq([income_history_1.id, income_history_2.id, income_history_3.id])
+            expect(results).to eq([income_history_1, income_history_2, income_history_3])
           end
         end
       end
 
       describe ".date_to" do
+        let!(:income_history_1) { FactoryGirl.create(:income_history, partner: partner, referral: user, created_at: 2.months.ago) }
+        let!(:income_history_2) { FactoryGirl.create(:income_history, partner: partner, referral: user, created_at: 1.month.ago) }
+        let!(:income_history_3) { FactoryGirl.create(:income_history, partner: partner, referral: user) }
 
+        context "when date_to provided" do
+          it "returns proper data by date_to" do
+            results = Referrals::IncomeHistory.by_date_to(1.month.ago)
+
+            expect(results).to eq([income_history_1, income_history_2])
+          end
+        end
+
+        context "when date_to not provided" do
+          it "returns all data" do
+            results = Referrals::IncomeHistory.by_date_from(nil)
+
+            expect(results).to eq([income_history_1, income_history_2, income_history_3])
+          end
+        end
+      end
+
+      describe ".by_partner" do
+        let!(:partner_user_2) { FactoryGirl.create(:user) }
+        let!(:partner_2) { FactoryGirl.create(:partner, user: partner_user_2) }
+        let!(:income_history_1) { FactoryGirl.create(:income_history, partner: partner, referral: user) }
+        let!(:income_history_2) { FactoryGirl.create(:income_history, partner: partner_2, referral: user) }
+        let!(:income_history_3) { FactoryGirl.create(:income_history, partner: partner, referral: user) }
+
+        it "returns data for partner" do
+          result = Referrals::IncomeHistory.by_partner(partner)
+
+          expect(result).to eq([income_history_1, income_history_3])
+        end
       end
 
     end
