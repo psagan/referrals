cbcee70b395182c06588b03bc35ecad355dcf3a3
psagan <patryk.sagan@icloud.com>
2017-01-06 09:23:44 +0100

    Add associations between withdrawal and partner

--- app/models/referrals/partner.rb
+++ app/models/referrals/partner.rb
@@ -2,6 +2,7 @@ module Referrals
   class Partner < ApplicationRecord
     has_many :referral_users
     has_many :referrals, through: :referral_users
+    has_many :withdrawals
     belongs_to :user, class_name: Referrals.user_class, foreign_key: :user_id
     has_many :income_histories
 
--- app/models/referrals/withdrawal.rb
+++ app/models/referrals/withdrawal.rb
@@ -1,5 +1,6 @@
 module Referrals
   class Withdrawal < ApplicationRecord
+    belongs_to :partner
     enum status: { pending: 0, paid: 1, cancelled: 2 }
   end
 end
