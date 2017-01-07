500f73f02d42acd26916cc030a4008428ba6af12
psagan <patryk.sagan@icloud.com>
2017-01-07 11:13:07 +0100

    Update seed examples

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -3,15 +3,22 @@ q = 10
 2.times do |i|
   email = "partner#{i}@example.com"
   User.create(email: email)
-  partner = User.find_by_email(email).tap {|u| u.make_partner!}
+  partner_user = User.find_by_email(email).tap {|u| u.make_partner!}
   q.times do |n|
-    created_at = n < (q/2) ? 1.month.ago : DateTime.now
     User.create(email: "m#{i}_#{n}@example.com")
     User.last.tap do |u|
-      partner.reload.partner.referrals << u
-      Referrals::CaptureReferralActionService.new(amount: Money.new(2077), referral: u, info: 'Payment for subscription').call
+      partner_user.reload.partner.referrals << u
+      Referrals::CaptureReferralActionService.new(amount: Money.new(20077), referral: u, info: 'Payment for subscription').call
     end
   end
+  # change date in half
+  partner_user.partner.income_histories.take(q/2).each { |income_history| income_history.update(created_at: 35.days.ago) }
+
+  # add withdrawals
+  4.times do
+    ::Referrals::CreateWithdrawalService.new(amount: 101.47, partner: partner_user.partner).call
+  end
+  partner_user.partner.withdrawals.take(2).each { |withdrawal| withdrawal.update(created_at: 34.days.ago) }
 end
 
 
