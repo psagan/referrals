dedb1a6c0956c3b0532b2b287f911da040d6beaf
psagan <patryk.sagan@icloud.com>
2017-01-12 10:12:00 +0100

    Handle partner amount + spec

--- app/services/referrals/update_withdrawal_service.rb
+++ app/services/referrals/update_withdrawal_service.rb
@@ -1,7 +1,9 @@
 module Referrals
   class UpdateWithdrawalService
+    extend Forwardable
     private
     attr_reader :withdrawal, :status
+    def_delegator :@withdrawal, :partner
 
     public
 
@@ -11,8 +13,9 @@ module Referrals
     end
 
     def call
+      return unless valid?
       withdrawal.transaction do
-        # @todo - handle partner amount
+        handle_partner_amount
         update_withdrawal
         # @todo - add withdrawal history
       end
@@ -20,14 +23,19 @@ module Referrals
 
     private
 
-    def update_withdrawal
-      case status
-        when 'pending' then withdrawal.pending!
-        when 'paid' then withdrawal.paid!
-        when 'cancelled' then withdrawal.cancelled!
-      end
+    def valid?
+      withdrawal.status != status && withdrawal.class.statuses.keys.include?(status)
     end
 
+    def handle_partner_amount
+      partner.decrease_amount(withdrawal.amount) if withdrawal.cancelled?
+      partner.increase_amount(withdrawal.amount) if status == 'cancelled'
+      partner.save if partner.changed?
+    end
+
+    def update_withdrawal
+      withdrawal.send('%s!' % status)
+    end
 
   end
 end
--- spec/services/referrals/update_withdrawal_service_spec.rb
+++ spec/services/referrals/update_withdrawal_service_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe Referrals::UpdateWithdrawalService do
 
   describe "#call" do
     let(:partner) { FactoryGirl.create(:partner, amount: 708.47) }
-    let(:withdrawal) { FactoryGirl.create(:withdrawal, status: :pending) }
+    let(:withdrawal) { FactoryGirl.create(:withdrawal, status: previous_status, amount: 127.38, partner: partner) }
     subject(:service) { described_class.new(withdrawal: withdrawal, status: status) }
+    let(:previous_status) { 'pending' }
     context "when proper status provided" do
-      context "when status paid" do
+      context "when new status paid" do
         let(:status) { 'paid' }
         it "set paid status" do
           service.call
@@ -20,18 +21,22 @@ RSpec.describe Referrals::UpdateWithdrawalService do
 
         context "when previous status pending" do
           it "does not change partner amount" do
-
+            expect { service.call }.to change { partner.amount }.by(0)
+            expect(partner.reload.amount).to eq(Money.new(70847))
           end
         end
 
         context "when previous status cancelled" do
+          let(:previous_status) { 'cancelled' }
           it "decreases partner amount" do
+            service.call
 
+            expect(partner.reload.amount).to eq(Money.new(58109))
           end
         end
       end
 
-      context "when status cancelled" do
+      context "when new status cancelled" do
         let(:status) { 'cancelled' }
         it "set cancelled status" do
           service.call
@@ -39,12 +44,25 @@ RSpec.describe Referrals::UpdateWithdrawalService do
           expect(withdrawal.reload.cancelled?).to eq(true)
         end
 
-        it "increases partner amount" do
+        context "when previous status pending" do
+          it "increases partner amount" do
+            service.call
 
+            expect(partner.reload.amount).to eq(Money.new(83585))
+          end
+        end
+
+        context "when previous status paid" do
+          let(:previous_status) { 'paid' }
+          it "increases partner amount" do
+            service.call
+
+            expect(partner.reload.amount).to eq(Money.new(83585))
+          end
         end
       end
 
-      context "when status pending" do
+      context "when new status pending" do
         let(:status) { 'pending' }
         before do
           withdrawal.paid!
@@ -54,33 +72,57 @@ RSpec.describe Referrals::UpdateWithdrawalService do
 
           expect(withdrawal.reload.pending?).to eq(true)
         end
-      end
 
-      context "when previous status paid" do
-        it "does not change partner amount" do
+        context "when previous status paid" do
+          let(:status) { 'paid' }
+          it "does not change partner amount" do
 
+          end
         end
-      end
 
-      context "when previous status cancelled" do
-        it "decreases partner amount" do
+        context "when previous status cancelled" do
+          let(:status) { 'cancelled' }
+          it "decreases partner amount" do
+            service.call
 
+            expect(partner.reload.amount).to eq(Money.new(83585))
+          end
         end
       end
     end
 
     context "when improper status provided" do
-      let(:status) { 'not_existing_status' }
-      it "does not change status" do
-        expect(withdrawal.pending?).to eq(true)
+      context "when unknown status" do
+        let(:status) { 'not_existing_status' }
+        it "does not change status" do
+          expect(withdrawal.pending?).to eq(true)
 
-        service.call
+          service.call
 
-        expect(withdrawal.pending?).to eq(true)
+          expect(withdrawal.pending?).to eq(true)
+        end
+
+        it "does not change partner amount" do
+          expect { service.call }.to change { partner.amount }.by(0)
+          expect(partner.reload.amount).to eq(Money.new(70847))
+        end
       end
 
-      it "does not change partner amount" do
+      context "when same status provided" do
+        let(:status) { 'pending' }
 
+        it "does not change status" do
+          expect(withdrawal.pending?).to eq(true)
+
+          service.call
+
+          expect(withdrawal.pending?).to eq(true)
+        end
+
+        it "does not change partner amount" do
+          expect { service.call }.to change { partner.amount }.by(0)
+          expect(partner.reload.amount).to eq(Money.new(70847))
+        end
       end
     end
   end
