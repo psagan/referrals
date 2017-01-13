f90ef0c00f597db49dace0ff9213deddbe674c2a
psagan <patryk.sagan@icloud.com>
2017-01-13 11:22:32 +0100

    Update helper to return empty string when status number is blank

--- app/helpers/referrals/withdrawal_helper.rb
+++ app/helpers/referrals/withdrawal_helper.rb
@@ -1,6 +1,7 @@
 module Referrals
   module WithdrawalHelper
     def withdrawal_status_by_number(number)
+      return '' if number.blank?
       t("activerecord.attributes.referrals/withdrawal.status.#{::Referrals::Withdrawal.statuses.key(number)}")
     end
   end
