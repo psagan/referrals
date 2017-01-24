72744a9d009326e47558262e50257402e386eced
psagan <patryk.sagan@icloud.com>
2017-01-24 07:16:50 +0100

    Update link helper

--- app/helpers/referrals/link_helper.rb
+++ app/helpers/referrals/link_helper.rb
@@ -3,7 +3,7 @@ module Referrals
     def referral_link(partner)
       # @todo - make configurable host and pid, if not config option use default
       # @todo - use request.protocol + request.host when no config option defined
-      request.protocol + request.host.chomp('/') + '/?pid=' + partner.id.to_s
+      request.protocol + request.host.chomp('/') + '?pid=' + partner.id.to_s
     end
   end
 end
